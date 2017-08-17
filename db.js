5455a778e39bb1e8adffdb4cbe5cccab8fe96d96
adnanrahic <ado.raha198@gmail.com>
2017-08-18 00:42:28 +0200

    Now connecting to the db based on config env;

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
-var mongoose = require('mongoose');
-/**
- * need to plug in native promises to work with promises instead of callbacks
- */
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/boilerplate-api', { useMongoClient: true });
+module.exports = function (app) {
+
+  var mongoose = require('mongoose');
+  /**
+   * need to plug in native promises to work with promises instead of callbacks
+   */
+  mongoose.Promise = global.Promise;
+  mongoose.connect(app.config.db, { useMongoClient: true });
+  
+}
