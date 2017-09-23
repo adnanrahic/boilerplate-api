7d94e95fc6460cd9cee9d6aa8d2f3b4d397d77a3
adnanrahic <ado.raha198@gmail.com>
2017-09-24 01:38:08 +0200

    Moving server config outside the module exports.

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+global.__root = __dirname + '/';
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.config = require('./config')(env);
+const db = require('./db')(app);
+const routes = require('./routes')(app);
+app.port = process.env.PORT || 3000;
+
 module.exports = {
   serve(env) {
     return new Promise((resolve, reject) => {
-      
-      const express = require('express');
-      const app = express();
-      const bodyParser = require('body-parser');
-      global.__root = __dirname + '/';
-      app.use(bodyParser.json());
-      app.use(bodyParser.urlencoded({ extended: true }));
-      app.config = require('./config')(env);
-      const db = require('./db')(app);
-      const routes = require('./routes')(app);
-      app.port = process.env.PORT || 3000;
-
       app.listen(app.port, function (err) {
         if (err) 
           return reject(err);
