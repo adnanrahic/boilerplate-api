68e4d6ac4371dd76388bae4901fa3118c4c596fc
adnanrahic <ado.raha198@gmail.com>
2017-10-24 00:23:18 +0200

    Added dotenv and winston logging

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
+require('dotenv').config({ path: './variables.env' });
 const server = require('./server');
-server.serve(process.env.NODE_ENV || 'dev')
-  .then(app => console.log('Server running on port', app.port))
-  .catch(err => console.error('Error in app.js at:', err));
+server.serve(process.env.NODE_ENV)
+  .then(app => logger.log('info', `Server running on port ${app.port}`))
+  .catch(err => logger.log('error', `Error in app.js at: ${err}`));
--- server.js
+++ server.js
@@ -1,15 +1,34 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, printf, splat } = format;
 global.__root = __dirname + '/';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+if (!fs.existsSync('logs')) {
+  fs.mkdirSync('logs');
+}
+
+global.logger = createLogger({
+  format: combine(
+    timestamp(),
+    printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
+    splat()
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new transports.File({ filename: 'logs/combined.log' })]
+});
+
 module.exports = {
   serve(env) {
     app.config = require('./config')(env);
-    const db = require('./db')(app);
-    const routes = require('./routes')(app);
+    require('./db')(app);
+    require('./routes')(app);
     app.port = process.env.PORT || 3000;
     return new Promise((resolve, reject) => {
       app.listen(app.port, function (err) {
@@ -20,4 +39,4 @@ module.exports = {
       });
     });
   }
-}
+};
