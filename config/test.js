f69986cdade0ce17484e8154db2b9fbce15083ba
adnanrahic <ado.raha198@gmail.com>
2017-10-24 00:25:06 +0200

    cleanup test config

--- config/test.js
+++ config/test.js
@@ -1,6 +1,6 @@
-const secret = process.env.secret || 'supersecret';
-const env = process.env.node_env || 'test';
-const db = process.env.db || 'mongodb://localhost:27017/boilerplate-api-test';
+const secret = process.env.SECRET || 'supersecret';
+const env = process.env.NODE_ENV || 'test';
+const db = process.env.DB || 'mongodb://localhost:27017/boilerplate-api-test';
 
 module.exports = {
   'secret': secret,
