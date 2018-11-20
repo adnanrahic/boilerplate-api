0be109040d9131d33be6c57812d485e2a18c5627
adnanrahic <ado.raha198@gmail.com>
2018-11-20 15:57:35 +0100

    (ci) fix

--- build_image_if_not_exists.sh
+++ build_image_if_not_exists.sh
@@ -1,3 +1,4 @@
 if [[ "$(docker images -q $API_IMAGE:$TAG 2> /dev/null)" == "" ]]; then
-  docker-compose build
+  docker build --tag $API_IMAGE:$TAG -f Dockerfile.builder .
+  docker build --tag $CLIENT_IMAGE:$TAG ./client
 fi
