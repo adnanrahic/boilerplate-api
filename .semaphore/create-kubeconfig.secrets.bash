1b228e90fb588683bf31104ef70e08b91e7a1c69
adnanrahic <ado.raha198@gmail.com>
2018-12-06 12:05:23 +0100

    (ci) trigger

--- .semaphore/create-kubeconfig.secrets.bash
+++ .semaphore/create-kubeconfig.secrets.bash
@@ -1,3 +1,5 @@
+KUBECONFIG=${HOME}/${KOPS_CLUSTER_NAME}-kubeconfig.yaml kops export kubecfg --name ${KOPS_CLUSTER_NAME} --state ${KOPS_STATE_STORE}
+
 sem create secret kubeconfig \
   --file ${HOME}/${KOPS_CLUSTER_NAME}-kubeconfig.yaml:/home/semaphore/.kube/config
 
