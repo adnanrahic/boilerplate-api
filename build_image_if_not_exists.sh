72a8cbd703951f8b653e467606befea3c5a92c28
adnanrahic <ado.raha198@gmail.com>
2018-12-21 15:10:06 +0100

    (ci) test build if not exists bash script

--- build_image_if_not_exists.sh
+++ build_image_if_not_exists.sh
@@ -1,3 +1,8 @@
-if [[ "$(docker images -q $API_IMAGE:$TAG 2> /dev/null)" == "" ]]; then
-  docker-compose -f docker-compose.build.yml build
+if [ -d "docker_images" ]; then 
+  docker load -i docker_images/api_image_${TAG}.tar
+  docker load -i docker_images/client_image_${TAG}.tar
+else 
+  if [[ "$(docker images -q $API_IMAGE:$TAG 2> /dev/null)" == "" ]]; then
+    docker-compose -f docker-compose.build.yml build
+  fi
 fi
